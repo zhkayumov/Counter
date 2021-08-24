dc2f95926cf3d306b88ecefd5a42232ea5fc696d
zhkayumov <72659573+zhkayumov@users.noreply.github.com>
2021-08-25 00:08:42 +0500

    Initial_commit

---
+++ Counter2/app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.counter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+
+    }
+    private lateinit var textView: TextView
+
+    fun count_me(view: View) {
+        val countString = textView.text.toString()
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+        // Display the new value in the text view.
+        textView.text = count.toString();
+
+    }
+}
